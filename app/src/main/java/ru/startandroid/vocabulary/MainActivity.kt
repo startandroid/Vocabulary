45793627c6454b0ebcbf61899b010a9421be6618
startandroid <startofandroid@gmail.com>
2017-06-26 00:58:48 +0300

    Main screen; dependencies for butterknife, dbflow, rxjava;

--- app/src/main/java/ru/startandroid/vocabulary/MainActivity.kt
+++ app/src/main/java/ru/startandroid/vocabulary/MainActivity.kt
@@ -1,12 +1,21 @@
 package ru.startandroid.vocabulary
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import butterknife.ButterKnife
 
 class MainActivity : AppCompatActivity() {
+
+    //@BindView(R.id.text2)
+    //lateinit var textView2: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+
+
     }
+
 }
